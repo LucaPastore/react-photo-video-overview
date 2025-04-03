42611721c7e28003963568405979cd429a25bb11
Pastore Luca <luca.pastore1@outlook.it>
2025-04-03 23:27:57 +0200

    fixing vercel deploy again

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["isomorphic-fetch"], // Seleziona le dipendenze esterne che non devono essere incluse nel bundle
+    }
+  },
   optimizeDeps: {
     include: ["isomorphic-fetch"]
   }
